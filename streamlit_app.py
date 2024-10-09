b3128958b9e9505eb61c97f38ea1ec666b90df5f
VincentQA <144799030+VincentQA@users.noreply.github.com>
2024-10-09 17:03:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,6 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 # Identifiants des assistants depuis les secrets
 ASSISTANT_IDS = {
  "Steve Jobs": st.secrets["ASSISTANT_ID_1"],
- "Jeff Bezos": st.secrets["ASSISTANT_ID_2"],
- "Elon Musk": st.secrets["ASSISTANT_ID_3"],
- "Mark Zuck": st.secrets["ASSISTANT_ID_4"],
 }
 
 # Initialisation de l'état de la session pour stocker l'historique des conversations et les threads
